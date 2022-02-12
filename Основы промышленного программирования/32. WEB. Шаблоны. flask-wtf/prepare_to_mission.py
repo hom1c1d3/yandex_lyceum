dec7d4727d5a97f2329af30b17e56af8d9c95342
TheMerret <merret@bk.ru>
2022-02-12 19:29:57 +0300

    init solution for prepare_to_mission

---
+++ Основы промышленного программирования/32. WEB. Шаблоны. flask-wtf/prepare_to_mission.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'title': 'MarsOne'})
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run("", 8080)

02f36a7a0a1cb73d3c4eacbc13eaf1615e731b13
TheMerret <merret@bk.ru>
2022-02-13 19:38:20 +0300

    upload_gallery form using wtf

--- Основы промышленного программирования/32. WEB. Шаблоны. flask-wtf/upload_gallery.py
+++ Основы промышленного программирования/32. WEB. Шаблоны. flask-wtf/upload_gallery.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -108,14 +108,20 @@ def get_image_paths(base_path):
     return [f"img/gallery/{i}" for i in os.listdir(base_path)]
 
 
+class GalleryForm(FlaskForm):
+    photo = FileField('Добавить картинку', validators=[DataRequired()])
+    submit = SubmitField('Загрузить')
+
+
 @app.route('/gallery', methods=['GET', 'POST'])
 def gallery():
     image_base_path = 'static/img/gallery'
     if request.method == 'GET':
+        form = GalleryForm()
         image_paths = get_image_paths(image_base_path)
-        return render_template('gallery.html', image_paths=image_paths)
+        return render_template('gallery.html', image_paths=image_paths, form=form)
     elif request.method == 'POST':
-        file = request.files['file']
+        file = request.files['photo']
         with open(os.path.join(image_base_path, file.filename), 'wb') as f:
             f.write(file.stream.read())
         return redirect('/gallery')

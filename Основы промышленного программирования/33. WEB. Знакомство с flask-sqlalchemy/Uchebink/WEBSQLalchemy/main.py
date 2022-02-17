7553e8d0ba018a525b429b76ab97eb9568ab73bd
TheMerret <merret@bk.ru>
2022-02-17 20:01:05 +0300

    Display news on page

--- Основы промышленного программирования/33. WEB. Знакомство с flask-sqlalchemy/Uchebink/WEBSQLalchemy/main.py
+++ Основы промышленного программирования/33. WEB. Знакомство с flask-sqlalchemy/Uchebink/WEBSQLalchemy/main.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from data.news import News
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRETTOKEN"
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private == False).all()
+    return render_template("index.html", news=news)
+
+
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    app.run("", port=8080)
 
 
 if __name__ == '__main__':

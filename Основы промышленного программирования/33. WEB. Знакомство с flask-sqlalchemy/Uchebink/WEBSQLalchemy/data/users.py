9d78f7dd014c8034b54edf8ba5380e16b41116d1
TheMerret <merret@bk.ru>
2022-02-17 19:09:27 +0300

    Added database with columns

---
+++ Основы промышленного программирования/33. WEB. Знакомство с flask-sqlalchemy/Uchebink/WEBSQLalchemy/data/users.py
@@ -0,0 +1,14 @@
+import sqlalchemy as sa
+from datetime import datetime
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'user'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=True)
+    about = sa.Column(sa.String, nullable=True)
+    email = sa.Column(sa.String, nullable=True, unique=True, index=True)
+    hashed_password = sa.Column(sa.String, nullable=True)
+    create_date = sa.Column(sa.DateTime, default=datetime.now)
+

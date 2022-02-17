7ea2c493cf09571f1464887b543c8d001233b3d8
TheMerret <merret@bk.ru>
2022-02-17 18:51:13 +0300

    Init new lesson with template of sqlalchemy.

---
+++ Основы промышленного программирования/33. WEB. Знакомство с flask-sqlalchemy/Uchebink/WEBSQLalchemy/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from data import db_session
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "SECRETTOKEN"
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+if __name__ == '__main__':
+    main()

1dc37b62aaef38df17802778cb1c3d13fb484e91
TheMerret <merret@bk.ru>
2022-02-22 20:18:34 +0300

    init solution for testing queen attack. (not working)

---
+++ Основы промышленного программирования/34. Библиотеки unittest и pytest/pytest_queen_attack.py
@@ -0,0 +1,52 @@
+import pytest
+from yandex_testing_lesson import is_under_queen_attack
+
+
+def test_position_out_of_board():
+    with pytest.raises(ValueError):
+        is_under_queen_attack("i8", "h8")
+        is_under_queen_attack("h9", "h8")
+
+
+def test_position_wrong_format():
+    with pytest.raises(ValueError):
+        is_under_queen_attack("h 8", "h8")
+        is_under_queen_attack("88", "h8")
+        is_under_queen_attack("abc", "h8")
+
+
+def test_position_wrong_type():
+    with pytest.raises(TypeError):
+        is_under_queen_attack((8, 8), "h8")
+        is_under_queen_attack(64, "h8")
+
+
+def test_queen_position_out_of_board():
+    with pytest.raises(ValueError):
+        is_under_queen_attack("h8", "i8")
+        is_under_queen_attack("h8", "h9")
+
+
+def test_queen_position_wrong_format():
+    with pytest.raises(ValueError):
+        is_under_queen_attack("h8", "h 8")
+        is_under_queen_attack("h8", "88")
+        is_under_queen_attack("h8", "abc")
+
+
+def test_queen_position_wrong_type():
+    with pytest.raises(TypeError):
+        is_under_queen_attack("h8", (8, 8))
+        is_under_queen_attack("h8", 64)
+
+
+def test_queen_can_beat_same_cell():
+    assert is_under_queen_attack("a1", "a1")
+
+
+def test_queen_can_beat():
+    assert is_under_queen_attack("a1", "h8")
+
+
+def test_queen_can_not_beat():
+    assert not is_under_queen_attack("a1", "g8")

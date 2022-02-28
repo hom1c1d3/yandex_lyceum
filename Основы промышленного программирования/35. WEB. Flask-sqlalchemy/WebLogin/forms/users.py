75e3c5bb8bd3f3fb0257003979e5cdb865a8bc3e
TheMerret <merret@bk.ru>
2022-02-28 23:19:31 +0300

    Added login form

--- Основы промышленного программирования/35. WEB. Flask-sqlalchemy/WebLogin/forms/users.py
+++ Основы промышленного программирования/35. WEB. Flask-sqlalchemy/WebLogin/forms/users.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField
+from wtforms import PasswordField, StringField, SubmitField, EmailField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, NumberRange
 from wtforms.validators import ValidationError
 
@@ -25,3 +25,10 @@ class RegisterForm(FlaskForm):
     speciality = StringField("Speciality")
     address = StringField("Address")
     submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')

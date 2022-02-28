1612910f30937df0349bc9eea051e7d53f4414d8
TheMerret <merret@bk.ru>
2022-02-28 19:19:32 +0300

    added logout

--- Основы промышленного программирования/35/Uchebnik/Web/main.py
+++ Основы промышленного программирования/35/Uchebnik/Web/main.py
@@ -71,6 +71,13 @@ def login():
     return render_template("login.html", title="Авторизация", form=form)
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
 # Пробуем запустить.
 
 """

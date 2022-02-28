09fa70b0756f9ba6cc224d4fb05193053f74c068
TheMerret <merret@bk.ru>
2022-02-28 19:13:11 +0300

    added login view

--- Основы промышленного программирования/35/Uchebnik/Web/main.py
+++ Основы промышленного программирования/35/Uchebnik/Web/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 from data.news import News
 from data.users import User
 from data import db_session
@@ -58,6 +58,19 @@ def load_user(user_id):
 # 2-5 class LoginForm: \data\users.py
 # 2-6 Шаблон: templates/login.html
 # 2-7 Сделаем обработчик адреса /login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html", message="Неправильный логин или пароль", form=form)
+    return render_template("login.html", title="Авторизация", form=form)
+
+
 # Пробуем запустить.
 
 """

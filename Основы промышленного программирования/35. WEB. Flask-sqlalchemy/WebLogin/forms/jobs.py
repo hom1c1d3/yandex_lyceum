942a7d9b041ff08ff250d5c0be7bb6892915a724
TheMerret <merret@bk.ru>
2022-03-01 22:57:00 +0300

    added job wtform

---
+++ Основы промышленного программирования/35. WEB. Flask-sqlalchemy/WebLogin/forms/jobs.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, BooleanField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class AddJobForm(FlaskForm):
+    job = StringField('Job Title', validators=[DataRequired()])
+    team_leader = IntegerField('Team Leader id', validators=[DataRequired()])
+    work_size = StringField('Work Size', validators=[DataRequired()])
+    collaborators = StringField('Collaborators', validators=[DataRequired()])
+    is_finished = BooleanField('Is job finished?')
+    submit = SubmitField('Submit')
